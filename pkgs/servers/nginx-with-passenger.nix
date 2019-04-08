ffd316d0d372ea671ce39b408762dbdaca396e3c
zetavg <mail@zeta.vg>
2019-04-09 06:31:43 +0800

    nginx-with-passenger: increase NGX_CONF_BUFFER to 128k to avoid too long parameter error

--- pkgs/servers/nginx-with-passenger.nix
+++ pkgs/servers/nginx-with-passenger.nix
@@ -6,4 +6,19 @@
   meta = oldAttrs.meta // {
     broken = stdenv.isDarwin;
   };
+  patches = (oldAttrs.patches or [ ]) ++ [
+    (builtins.toFile "patch" ''
+      --- ./src/core/ngx_conf_file.c
+      +++ ./src/core/ngx_conf_file.c
+      @@ -8,7 +8,7 @@
+       #include <ngx_config.h>
+       #include <ngx_core.h>
+
+      -#define NGX_CONF_BUFFER  4096
+      +#define NGX_CONF_BUFFER  131072
+
+       static ngx_int_t ngx_conf_add_dump(ngx_conf_t *cf, ngx_str_t *filename);
+       static ngx_int_t ngx_conf_handler(ngx_conf_t *cf, ngx_int_t last);
+    '')
+  ];
 }) // { passenger = nginx-mod-passenger.passenger; }
