a5166683bef88103c4cda3617297f4ae0f903877
zetavg <mail@zeta.vg>
2020-04-02 22:21:48 +0800

    passenger: add sha256 for nginx-1.16.1

--- pkgs/servers/passenger/default.nix
+++ pkgs/servers/passenger/default.nix
@@ -32,6 +32,8 @@
       "0i88fp81k4pq1rqpq6hq6qdrrr48cim1xz2qcpchpmr1cy83sjps";
     "https://nginx.org/download/nginx-1.15.8.tar.gz" =
       "11q7njr0khv8hb96bclyw5f75gvm12nw3jjgmq9rifbym2yazgd8";
+    "https://nginx.org/download/nginx-1.16.1.tar.gz" =
+      "0az3vf463b538ajvaq94hsz9ipmjgnamfj1jy0v5flfks5njl77i";
   }
 }:
 
